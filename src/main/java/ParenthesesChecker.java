2bce74840b78900b90a537ad042dda2ddc90dbcf
Carla022 <cs.morenol@outlook.com>
2022-08-25 18:31:24 +0100

    add if and else if statements

--- src/main/java/ParenthesesChecker.java
+++ src/main/java/ParenthesesChecker.java
@@ -4,19 +4,35 @@ import java.util.Stack;
 public class ParenthesesChecker {
 
 //METHOD: Checking parentheses-----
-    public boolean checkParentheses(String checker) {
+    public boolean checkParentheses(String string) {
         //    Create a Stack of Characters
-        Stack<Character> stackOfCharacters = new Stack<>();
+        Stack<Character> checker = new Stack<>();
+
 //       Move through the String = Character Array, 0 based index (i = 0)
-
-
-        for (int i = 0; i < checker.length(); i++) {
+        for (int i = 0; i < string.length(); i++)
+        {
 
         }
+
 //      character index is stored in "indicator" variable when looping
 //      indicator 0 = specific character at index 0, indicator 1 = specific character at index 1...
+        char indicator = string.charAt(0);
 
-        char indicator = checker.charAt(i);
+//        if expression[i] is '(' or '{' or '[', push (expression [i]) into the stack (will stack like pancake)
+
+        if(indicator == '(' || indicator == '{' || indicator == '['){
+            checker.push (indicator);
+        }
+//        else if expression [i] is ')' or '}' or ']',
+        else if(indicator == ')' || indicator == '}' || indicator == ']'){
+            return false;
+
+        }
+//        if (stack is empty) || (top does not pair with the expression [i]),
+//        Return false.
+//        Else, Pop (),
+//        Return Stack is empty ? true : false
+
 
 
 

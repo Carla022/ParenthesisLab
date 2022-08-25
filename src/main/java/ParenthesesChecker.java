c586baae1f627bbfefa8760547c4b6b6be0184ff
Carla022 <cs.morenol@outlook.com>
2022-08-25 14:28:06 +0100

    first Class edit

---
+++ src/main/java/ParenthesesChecker.java
@@ -0,0 +1,12 @@
+public class ParenthesesChecker {
+
+    public boolean checker (String testString){
+        String ParenthesesChecker = new ParenthesesChecker();
+
+
+    // Create a reference variable stack (A collection of items)
+    // this stack will accept datatype Character- (Using Characters so I don't have to chop  the string )
+    // + this stack is called "stackWithStrings"
+
+    }
+}

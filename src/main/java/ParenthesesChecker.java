cfaee429ccac82f5ddf6655b9c16f2f9908901ef
Carla022 <cs.morenol@outlook.com>
2022-08-25 21:25:26 +0100

    add test pass

--- src/main/java/ParenthesesChecker.java
+++ src/main/java/ParenthesesChecker.java
@@ -1,46 +1,57 @@
-import java.sql.SQLData;
 import java.util.Stack;
-
 public class ParenthesesChecker {
 
-//METHOD: Checking parentheses-----
+    // METHOD: Checking parentheses-----
+
     public boolean checkParentheses(String string) {
-        //    Create a Stack of Characters
-        Stack<Character> checker = new Stack<>();
+//    Create a Stack of Characters
+        Stack<Character> stackOfCharacters= new Stack<>();
 
 //       Move through the String = Character Array, 0 based index (i = 0)
-        for (int i = 0; i < string.length(); i++)
-        {
-
-        }
+        for (int i = 0; i < string.length(); i++) {
 
 //      character index is stored in "indicator" variable when looping
 //      indicator 0 = specific character at index 0, indicator 1 = specific character at index 1...
-        char indicator = string.charAt(0);
+            char indicator = string.charAt(i);
 
 //        if expression[i] is '(' or '{' or '[', push (expression [i]) into the stack (will stack like pancake)
+            if (indicator == '(' || indicator == '[' || indicator == '<' || indicator == '{'){
+                stackOfCharacters.push(indicator);
 
-        if(indicator == '(' || indicator == '{' || indicator == '['){
-            checker.push (indicator);
-        }
 //        else if expression [i] is ')' or '}' or ']',
-        else if(indicator == ')' || indicator == '}' || indicator == ']'){
-            return false;
+            } else if (indicator == ')' || indicator == '}' || indicator == ']'){
 
-        }
-//        if (stack is empty) || (top does not pair with the expression [i]),
+//        if (stack is empty: means if there is no opening in stack) || (top does not (! means NOT) pair with the expression [i])
+//         PEEK method will look at the top item (latest) that was added in the stack...
+                if (stackOfCharacters.isEmpty()){
 //        Return false.
-//        Else, Pop (),
-//        Return Stack is empty ? true : false
+                    return false;
 
+//                    otherwise if...
+                } else {
 
+//from the stack, see if top character is equal paired characters (open + close) or... and if true, pop it.
+                    if     ((stackOfCharacters.peek() == '(' && indicator == ')') ||
+                            (stackOfCharacters.peek() == '{' && indicator == '}') ||
+                            (stackOfCharacters.peek() == '[' && indicator == ']')) {
 
+                        stackOfCharacters.pop();
 
-
-
-
-
-
+                    }
+                }
+            }
 
         }
+
+        if (stackOfCharacters.empty()){
+            return true;
+
+
+        } else {
+            return false;
+        }
+    }
+
 }
+
+

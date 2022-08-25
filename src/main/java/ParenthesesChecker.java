36502bcdef479b8b5734c0908d17c6efa27da7ea
Carla022 <cs.morenol@outlook.com>
2022-08-25 18:03:17 +0100

    Declare Stack and for loop

--- src/main/java/ParenthesesChecker.java
+++ src/main/java/ParenthesesChecker.java
@@ -1,12 +1,30 @@
+import java.sql.SQLData;
+import java.util.Stack;
+
 public class ParenthesesChecker {
 
-    public boolean checker (String testString){
-        String ParenthesesChecker = new ParenthesesChecker();
+//METHOD: Checking parentheses-----
+    public boolean checkParentheses(String checker) {
+        //    Create a Stack of Characters
+        Stack<Character> stackOfCharacters = new Stack<>();
+//       Move through the String = Character Array, 0 based index (i = 0)
 
 
-    // Create a reference variable stack (A collection of items)
-    // this stack will accept datatype Character- (Using Characters so I don't have to chop  the string )
-    // + this stack is called "stackWithStrings"
+        for (int i = 0; i < checker.length(); i++) {
 
-    }
+        }
+//      character index is stored in "indicator" variable when looping
+//      indicator 0 = specific character at index 0, indicator 1 = specific character at index 1...
+
+        char indicator = checker.charAt(i);
+
+
+
+
+
+
+
+
+
+        }
 }
